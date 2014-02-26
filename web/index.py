6f8448026d667266513e3de53caf5d47a6fc8b99
Rex <ai-rex@qq.com>
2014-02-26 20:14:44 +0800

    首页登录状态自动跳转用户页，用户页增加登出

--- web/index.py
+++ web/index.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
 
 from yag import page
+from yag import auth
 
 p = page.Page()
 
 p.add_header('Content-type: text/html; charset=utf8')
 
-p.add_file('template/index.html')
+if auth.cookie_auth(p.http_cookie):
+    p.redirect('user.py')
+else:
+    p.add_file('template/index.html')
 
 p.display()
 
